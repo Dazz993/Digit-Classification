d2892aaf93258e1d7673dbaeb7490bec92ab71f9
Dazz993 <dazz993@sjtu.edu.cn>
2021-06-16 09:30:38 +0800

    fix bugs

--- main_logistic_regression_binary_classifier.py
+++ main_logistic_regression_binary_classifier.py
@@ -14,45 +14,53 @@ parser = argparse.ArgumentParser(description='Pytorch CelebA Implementation')
 parser.add_argument('--cfg', default='', type=str, metavar='PATH',
                     help='path to configuration (default: none)')
 
-def train_one_epoch(model_id, model, criterion, optimizer, epoch, train_dataloader, cfg):
+def train_one_epoch(models, criterion, optimizers, epoch, train_dataloader, cfg):
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
     accs = AverageMeter()
 
-    model.train()
-
-    end = t()
-
     for batch_idx, (input, target) in enumerate(train_dataloader):
-        # process data loading time
-        data_time.update(t() - end)
-
-        # model execution
         input = input.to(device)
-        target = torch.where(target == model_id, 1, 0).to(device)
-        target = target.reshape(-1, 1).float()
 
-        output = model(input)
+        for i in range(10):
+            model = models[i]
+            optimizer = optimizers[i]
 
-        loss = criterion(output, target)
+            model.train()
 
-        # backpropagation
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+            end = t()
 
-        # process output and other measurements
-        loss = loss.float()
-        # output = output.detach().cpu().numpy()
-        # target = target.detach().cpu().numpy()
-        acc = binary_classifier_accuracy(output=output, y_true=target)
-        losses.update(loss, input.size(0))
-        accs.update(acc, input.size(0))
+            # process data loading time
+            data_time.update(t() - end)
 
-        # process batch time
-        batch_time.update(t() - end)
-        end = t()
+            # model execution
+            _target = torch.where(target == i, 1, 0).to(device)
+            # _target = _target.reshape(-1, 1).float()
+            # _target = _target.long()
+
+            print(_target.max(), _target.min())
+
+            output = model(input)
+
+            loss = criterion(output, _target)
+
+            # backpropagation
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # process output and other measurements
+            loss = loss.float()
+            # output = output.detach().cpu().numpy()
+            # target = target.detach().cpu().numpy()
+            acc = binary_classifier_accuracy(output=output, y_true=_target)
+
+            losses.update(loss, input.size(0))
+            accs.update(acc, input.size(0))
+
+            # process batch time
+            batch_time.update(t() - end)
 
         if batch_idx % cfg.print_frequency == 0:
             print(f'Epoch: [{epoch}]/[{cfg.epochs}],\t'
@@ -62,86 +70,73 @@ def train_one_epoch(model_id, model, criterion, optimizer, epoch, train_dataload
                   f'Loss: {losses.val:.4f} ({losses.avg:.4f}),\t'
                   f'Acc: {accs.val:.4f} ({accs.avg:.4f})')
 
-    return losses.avg, accs.avg
+    return losses.avg, accs.avg, [models[i].state_dict() for i in range(10)]
 
 @torch.no_grad()
-def validate(model_id, model, criterion, epoch, test_dataloader, cfg):
-    batch_time = AverageMeter()
-    data_time = AverageMeter()
-    losses = AverageMeter()
-    accs = AverageMeter()
-
-    model.eval()
-
-    end = t()
+def validate(models, criterion, epoch, test_dataloader, cfg):
+    losses = [AverageMeter() for _ in range(10)]
+    accs = [AverageMeter() for _ in range(10)]
 
     for batch_idx, (input, target) in enumerate(test_dataloader):
-        # process data loading time
-        data_time.update(t() - end)
 
-        # model execution
-        input = input.to(device)
-        target = torch.where(target == model_id, 1, 0).to(device)
-        target = target.reshape(-1, 1).float()
-        output = model(input)
-        loss = criterion(output, target)
+        for i in range(10):
+            model = models[i]
+            model.eval()
 
-        # process output and other measurements
-        loss = loss.float()
-        # output = output.detach().cpu().numpy()
-        # target = target.detach().cpu().numpy()
-        acc = binary_classifier_accuracy(output=output, y_true=target)
-        losses.update(loss, input.size(0))
-        accs.update(acc, input.size(0))
+            # model execution
+            input = input.to(device)
+            _target = torch.where(target == i, 1, 0).to(device)
+            # _target = _target.reshape(-1, 1).float()
+            # _target = _target.long()
 
-        # process batch time
-        batch_time.update(t() - end)
-        end = t()
+            output = model(input)
+            loss = criterion(output, _target)
 
-        # print(f'Epoch: [{epoch}]/[{cfg.epochs}]\t'
-        #       f'Batch: [{batch_idx}]/[{len(val_dataloader)}]\t'
-        #       f'Time {batch_time.val:.4f} ({batch_time.avg:.4f})\t'
-        #     #   f'Data Time {data_time.val:.4f} ({data_time.avg:.4f})\t'
-        #       f'Loss {losses.val:.4f} ({losses.avg:.4f})\t'
-        #       f'f1_score {f1_micro.val:.4f} {f1_macro.val:.4f}')
+            # process output and other measurements
+            loss = loss.float()
+            # output = output.detach().cpu().numpy()
+            # target = target.detach().cpu().numpy()
+            acc = binary_classifier_accuracy(output=output, y_true=_target)
+            losses[i].update(loss, input.size(0))
+            accs[i].update(acc, input.size(0))
 
-    print(f'* Epoch: {epoch}, Acc: {accs.avg:.4f}, test_loss: {losses.avg:.4f}\n')
+            # print(f'Epoch: [{epoch}]/[{cfg.epochs}]\t'
+            #       f'Batch: [{batch_idx}]/[{len(val_dataloader)}]\t'
+            #       f'Loss {losses.val:.4f} ({losses.avg:.4f})\t')
 
-    return losses.avg, accs.avg
+    average_accuracy = sum([accs[i].avg for i in range(10)]) / 10
+    average_losses = sum([losses[i].avg for i in range(10)]) / 10
 
-def train(model_id, model, criterion, optimizer, train_dataloader, test_dataloader, cfg):
-    best_test_acc, best_test_loss = 0, float('inf')
-    best_state_dict = model.state_dict()
+    print(f'* Epoch: {epoch}, Acc: {average_accuracy:.4f}, test_loss: {average_losses:.4f}\n')
+
+    return [losses[i].avg for i in range(10)], [accs[i].avg for i in range(10)]
+
+def train(models, criterion, optimizers, train_dataloader, test_dataloader, cfg):
+    best_test_acc = [0 for _ in range(10)]
+    best_test_loss = [float('inf') for _ in range(10)]
+    best_state_dicts = [models[i].state_dict() for i in range(10)]
     for epoch in range(cfg.epochs):
-        train_loss, train_acc = train_one_epoch(model_id=model_id, model=model, criterion=criterion, optimizer=optimizer, epoch=epoch, train_dataloader=train_dataloader, cfg=cfg)
-        test_loss, test_acc = validate(model_id=model_id, model=model, criterion=criterion, epoch=epoch, test_dataloader=test_dataloader, cfg=cfg)
+        train_loss, train_acc, model_state_dicts = train_one_epoch(models=models, criterion=criterion, optimizers=optimizers, epoch=epoch, train_dataloader=train_dataloader, cfg=cfg)
+        test_losses, test_acces = validate(models=models, criterion=criterion, epoch=epoch, test_dataloader=test_dataloader, cfg=cfg)
 
-        # if cfg.multigpu:
-        #     save_dict = {
-        #         'epoch': epoch + 1,
-        #         'loss': test_loss,
-        #         'score': test_acc,
-        #         'state_dict': model.module.state_dict()
-        #     }
-        # else:
-        #     save_dict = {
-        #         'epoch': epoch + 1,
-        #         'loss': test_loss,
-        #         'score': test_acc,
-        #         'state_dict': model.state_dict()
-        #     }
+        for i in range(10):
+            if test_losses[i] < best_test_loss[i]:
+                best_test_acc[i] = test_acces[i]
+                best_test_loss[i] = test_losses[i]
+                best_state_dicts[i] = model_state_dicts[i]
 
-        is_best = False
-        if test_acc > best_test_acc:
-            best_test_acc = test_acc
-            best_test_loss = test_loss
-            best_state_dict = model.state_dict()
-            is_best = True
-        # save_checkpoint(epoch, save_dict, is_best, cfg, path=cfg.checkpoint_path)
+        save_dict = {
+            'epoch': epoch + 1,
+            'loss': best_test_loss,
+            'score': best_test_acc,
+            'state_dicts': best_state_dicts
+        }
 
-    print(f"Model ID: {model_id}, Best test loss: {best_test_loss:.8f}, best test accuracy: {best_test_acc:.8f}\n")
+        test_for_all(models=models, model_state_dicts=best_state_dicts, test_dataloader=test_dataloader, cfg=cfg)
 
-    return best_state_dict
+        save_checkpoint(epoch, save_dict, True, cfg, path=cfg.checkpoint_path)
+
+    return best_test_acc, best_test_loss, best_state_dicts
 
 @torch.no_grad()
 def test_for_all(models, model_state_dicts, test_dataloader, cfg):
@@ -195,18 +190,18 @@ if __name__ == '__main__':
     input_size = train_dataset[0][0].shape
     models = []
     for i in range(10):
-        models.append(LinearModel(input_size=input_size, num_classes=1).to(device))
+        model = LinearModel(input_size=input_size, num_classes=1).to(device)
+        models.append(model)
 
     # define criterion
-    criterion = torch.nn.BCEWithLogitsLoss()
+    # criterion = torch.nn.BCEWithLogitsLoss()
+    criterion = torch.nn.NLLLoss()
     # define optimizer
     optimizers = []
     for i in range(10):
-        optimizers.append(torch.optim.AdamW(models[i].parameters(), lr=cfg.learning_rate))
+        optimizer = torch.optim.AdamW(models[i].parameters(), lr=cfg.learning_rate)
+        optimizers.append(optimizer)
 
-    model_state_dicts = []
-    for i in range(10):
-        state_dict = train(model_id=i, model=models[i], criterion=criterion, optimizer=optimizers[i], train_dataloader=train_dataloader, test_dataloader=test_dataloader, cfg=cfg)
-        model_state_dicts.append(state_dict)
+    best_test_acc, best_test_loss, best_state_dicts = train(models=models, criterion=criterion, optimizers=optimizers, train_dataloader=train_dataloader, test_dataloader=test_dataloader, cfg=cfg)
 
-    test_for_all(models=models, model_state_dicts=model_state_dicts, test_dataloader=test_dataloader, cfg=cfg)
+    test_for_all(models=models, model_state_dicts=best_state_dicts, test_dataloader=test_dataloader, cfg=cfg)
